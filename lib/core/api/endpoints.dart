f1126c85d73b4c5b80f34b5bb347f6197c54e00b
Abdu Saleem <saleemsaly8@gmail.com>
2023-10-28 23:13:58 +0530

    ios test

---
+++ lib/core/api/endpoints.dart
@@ -0,0 +1,6 @@
+const baseurl = 'http://shop.pallavinamdev.com/PHP_CollApp/';
+const loginurl = 'p_user_login.php';
+const datakey = 'R9WM1wN2ZDM5zZw+NQ==';
+const goldrateurl = 'p_user_getGoldRate.php';
+const schemedetailurl = 'p_user_schemeDetail.php';
+const userschemesurl = 'p_user_cusSchemes.php';
